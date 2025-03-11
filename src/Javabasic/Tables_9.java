0103f4d0776f02c3eaa1ac8396e3c959f5aa0cd1
Kanthi <poohkanthi@gmail.com>
2025-03-11 22:08:07 +0530

    Pushing table 9

---
+++ src/Javabasic/Tables_9.java
@@ -0,0 +1,10 @@
+package Javabasic;
+
+public class Tables_9 {
+    public static void main(String[] args) {
+        int n=9;
+        for(int i=1 ; i<11; i++){
+            System.out.println(n + "*" + i + "=" +n*i);
+        }
+    }
+}
